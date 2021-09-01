564ce673cb6a80febd6fe1581aa8c41dfc785479
tritims <tritims@gmail.com>
2021-09-01 11:52:00 +0530

    Added usage instructions

--- usage.py
+++ usage.py
@@ -1,3 +1,3 @@
 import hyperlink_grabber as hg
 
-hg.hyperlinks2csv('demo.docx', 'results.csv')
+hg.hyperlinks2csv('demo.docx', 'results.csv') # Saves all hyperlink text and their url from demo.docx in results.csv
